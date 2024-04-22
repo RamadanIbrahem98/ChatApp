f847adf683c07f1bb707ea70cb3956828f9682d9
Ramadan Ibrahim <ramadan.ibrahem98@eng-st.cu.edu.eg>
2024-04-22 16:46:30 +0200

    refactor(SidekiqConfig): change the order of the queues

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379') }
-  config.queues = %w(default applications chats messages)
+  config.queues = %w(chats messages default)
 end
 
 Sidekiq.configure_client do |config|
