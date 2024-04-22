8c0a16337dec9f4d0ed3ccc02d3905226c697d1e
Ramadan Ibrahim <ramadan.ibrahem98@eng-st.cu.edu.eg>
2024-04-22 17:38:11 +0200

    feat(SidekiqCron): create the job to run every hour

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,10 @@
+require 'sidekiq/cron'
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379') }
   config.queues = %w(chats messages default)
+
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file('config/sidekiq-cron.yml') if File.exist?('config/sidekiq-cron.yml')
 end
 
 Sidekiq.configure_client do |config|
