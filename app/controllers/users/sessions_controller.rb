15ee4d549827783bbef9b4fcf4d313cbd4ab37aa
Ramadan Ibrahim <ramadan.ibrahem98@eng-st.cu.edu.eg>
2024-04-23 17:28:05 +0200

    fix(SessionsController): change the login function to send the error incase of invalid credentials

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,16 +1,27 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
-  private  
+  private
     def respond_with(resource, _opts = {})
-      puts resource.inspect, 'resource'
+      if resource.username?
+        log_in_success
+      else
+        log_in_failure
+      end
+    end
+
+    def log_in_success
       render json: { message: 'Logged in.', data: UserSerializer.new(resource).serializable_hash[:data][:attributes] }, status: :ok
+    end
+
+    def log_in_failure
+      render json: { message: 'Invalid email or password.' }, status: :unauthorized
     end
 
     def respond_to_on_destroy
       current_user ? log_out_success : log_out_failure
     end
-    
+
     def log_out_success
       render json: { message: "Logged out." }, status: :ok
     end
