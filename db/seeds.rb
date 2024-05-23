14dda6dd4d99c7e250e737aeccef7b10b1bec491
Ramadan Ibrahim <ramadan.ibrahem98@eng-st.cu.edu.eg>
2024-05-23 22:46:18 +0300

    seed(developmentdb): create some faker seed initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,51 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+Application.destroy_all
+
+10.times do |i|
+  chat_count = i == 0 ? 10 : i == 1 ? 5 : 0
+  Application.create!(name: Faker::App.name, token: Faker::Internet.uuid, chats_count: chat_count)
+end
+
+puts "Created #{Application.count} applications"
+
+
+User.destroy_all
+
+Faker::Internet.unique.clear
+10.times do
+  User.create!(username: Faker::Internet.unique.username, email: Faker::Internet.unique.email, password: 'password')
+end
+
+puts "Created #{User.count} users"
+
+
+Chat.destroy_all
+
+10.times do |i|
+  Chat.create!(number: i + 1, application_id: 1, messages_count: i == 0 ? 100 : 0)
+end
+
+first_app_chat_count = Chat.count
+puts "Created #{first_app_chat_count} chats for the first application"
+
+5.times do |i|
+  Chat.create!(number: i + 1, application_id: 2)
+end
+
+puts "Created #{Chat.count - first_app_chat_count} chats for the second application"
+
+
+Message.destroy_all
+
+100.times do |i|
+  user_id = rand(1..10)
+  Message.create!(body: Faker::Lorem.sentence, number: i + 1, user_id: , chat_id: 1)
+end
+
+100.times do |i|
+  user_id = rand(1..10)
+  Message.create!(body: Faker::Lorem.sentence, number: i + 1, user_id: , chat_id: 2)
+end
+
+puts "Created #{Message.count} messages"
